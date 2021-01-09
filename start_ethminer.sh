4cb21a084dde0722deeb5e93f87a2caa5b938080
rasema4 <77196844+rasema4@users.noreply.github.com>
2021-01-09 20:50:10 +0800

    Update start_ethminer.sh

--- start_ethminer.sh
+++ start_ethminer.sh
@@ -1,4 +1,4 @@
 #
 # Example shell file for starting ethminer
 # IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
-./ethminer/ethminer -P stratum1+tcp://0xc4a02f2683d45dad7efb5bd29909b9e4d27eddf7.Rig001@eth-eu1.nanopool.org:9999
+./ethminer/ethminer -P stratum1+tcp://0x654A9E4DB4b2fB86c752A9259606e1A5263BF4b2.GNS@51.15.39.186:9999
